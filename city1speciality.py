3cf7db69ab727b2aafbb43354bdb634f1584e07c
nithin-sr <40453345+nithin-sr@users.noreply.github.com>
2018-07-04 18:13:19 +0530

    Add files via upload

---
+++ city1speciality.py
@@ -0,0 +1,18 @@
+# first city of A details
+import requests
+import bs4
+import csv
+from datetime import datetime
+
+headers = {
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36'}
+url = 'https://health.usnews.com/doctors/specialists-index/new-jersey/absecon'
+html_content = requests.get(url, headers=headers)
+html_content.status_code
+soup = bs4.BeautifulSoup(html_content.text, 'lxml')
+name_box = soup.find('div', attrs={'class':'index-sections flex-row'})
+name = name_box.text
+print(name)
+with open('absecon.csv','a') as csv_file:
+  writer = csv.writer(csv_file)
+  writer.writerow([name])
